6cc1009b93c17fca6d7d97b8b4740d68f11419e9
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-03-16 11:48:58 +0100

    Primer desarrollo realizado

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -7,6 +7,18 @@ import { setChartCanvas, setChartCanvasImage, setCustomCanvas, setChartCustomCan
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
+//Colores fijos
+const COLOR_PRIMARY_1 = '#F8B05C', 
+COLOR_PRIMARY_2 = '#E37A42', 
+COLOR_ANAG_1 = '#D1834F', 
+COLOR_ANAG_2 = '#BF2727', 
+COLOR_COMP_1 = '#528FAD', 
+COLOR_COMP_2 = '#AADCE0', 
+COLOR_GREY_1 = '#B5ABA4', 
+COLOR_GREY_2 = '#64605A', 
+COLOR_OTHER_1 = '#B58753', 
+COLOR_OTHER_2 = '#731854';
+
 export function initChart(iframe) {
 
     //Lectura de datos
@@ -16,14 +28,92 @@ export function initChart(iframe) {
         //Desarrollo del gráfico
         //Nos quedamos con los datos de 2019 > Están todos los países representados
         let dataUE = data.filter(function(item) { if(item.is_EU == 'YES') { return item; }});
-        console.log(dataUE);
+        
+        let margin = {top: 10, right: 10, bottom: 80, left: 30},
+            width = document.getElementById('chart').clientWidth - margin.left - margin.right,
+            height = document.getElementById('chart').clientHeight - margin.top - margin.bottom;
+
+        let svg = d3.select("#chart")
+            .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        let paises = d3.map(dataUE, function(d){return(d.NAME_PAIS)}).keys();
+        let tipos = ['Hombres', 'Mujeres'];
+        
+        let x = d3.scaleBand()
+            .domain(paises)
+            .range([0, width])
+            .padding([0.35]);
+
+        let xAxis = function(g) {
+            g.call(d3.axisBottom(x));
+            g.call(function(svg) {
+                svg.selectAll("text")	
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)")
+                .style("font-weight", function(d) {console.log(d); if(d == 'España' || d == 'UE-27') { return '700'} else { return '400'; }})
+            });
+        }
+
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        let y = d3.scaleLinear()
+            .domain([0, 25])
+            .range([ height, 0 ]);
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        let xSubgroup = d3.scaleBand()
+            .domain(tipos)
+            .range([0, x.bandwidth()])
+            .padding([0]);
+
+        let color = d3.scaleOrdinal()
+            .domain(tipos)
+            .range([COLOR_PRIMARY_1, COLOR_COMP_1]);
 
         function init() {
-
+            svg.append("g")
+                .selectAll("g")
+                .data(dataUE)
+                .enter()
+                .append("g")
+                .attr("transform", function(d) { return "translate(" + x(d.NAME_PAIS) + ",0)"; })
+                .selectAll("rect")
+                .data(function(d) { return tipos.map(function(key) { return {key: key, value: d[key]}; }); })
+                .enter()
+                .append("rect")
+                .attr('class', 'prueba')
+                .attr("x", function(d) { return xSubgroup(d.key); })
+                .attr("width", xSubgroup.bandwidth())
+                .attr("fill", function(d) { return color(d.key); })
+                .attr("y", function(d) { return y(0); })                
+                .attr("height", function(d) { return height - y(0); })
+                .transition()
+                .duration(2000)
+                .attr("y", function(d) { return y(d.value); })                
+                .attr("height", function(d) { return height - y(d.value); });
+                
         }
 
         function animateChart() {
-
+            svg.selectAll(".prueba")
+                .attr("x", function(d) { return xSubgroup(d.key); })
+                .attr("width", xSubgroup.bandwidth())
+                .attr("fill", function(d) { return color(d.key); })
+                .attr("y", function(d) { return y(0); })                
+                .attr("height", function(d) { return height - y(0); })
+                .transition()
+                .duration(2000)
+                .attr("y", function(d) { return y(d.value); })                
+                .attr("height", function(d) { return height - y(d.value); });
         }
 
         //////
